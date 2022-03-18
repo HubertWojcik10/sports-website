0530afaeada06d821bfd5aadcfd6383695754d3a
Hubert <hubert.wojcik002@gmail.com>
2022-03-18 18:23:34 +0100

    footer finished

---
+++ components/footer/Icon.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Icon = ({path}) => {
+  return (
+    <div className='text-gray-500 hover:text-gray-900 dark:hover:text-white'>
+        <Link href=''>
+            <svg className='w-5 h-5' fill='currentColor' viewBox='0 0 24 24' aria-hidden='true'>
+                <path fill-rule='evenodd' d={path} clip-rule='evenodd'></path>
+            </svg>
+        </Link>
+    </div>
+  )
+}
+
+export default Icon
