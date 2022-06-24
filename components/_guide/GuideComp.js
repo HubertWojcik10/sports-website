d38bcbc5507547c5d2f525cc74ad3db05dd98b44
Hubert <hubert.wojcik002@gmail.com>
2022-06-24 12:26:29 +0200

    guide comp improvement

--- components/_guide/GuideComp.js
+++ components/_guide/GuideComp.js
@@ -2,14 +2,27 @@ import React from 'react'
 import Header from '../header/Header'
 import PageTitle from '../universal/pages/PageTitle'
 import PageDescription from '../universal/pages/PageDescription'
+import PageSubTitle from '../universal/pages/PageSubTitle'
+import data from '../../data/pages/guide.json'
 
 const GuideComp = () => {
   return (
     <>
         <Header />
         <main className='lg:w-4/5 mx-auto py-10 px-3 min-h-screen pt-40'>
-            <PageTitle title='Poradnik' />
-            <PageDescription description='' />
+            <PageTitle title={data.title} />
+            <PageSubTitle subtitle={data.subtitle} />
+            <PageDescription description={data.description} />
+
+            {
+              data.guide.map((item, index) => {
+                //TODO
+                return (
+                  <div key={index} className='flex flex-col items-center justify-center'>
+                    {item.title}
+                  </div>)
+              })
+            }
          </main>
     </>
   )
--- components/universal/pages/PageSubTitle.js
+++ components/universal/pages/PageSubTitle.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PageSubTitle = ({subtitle, styles=''}) => {
   return (
-    <div className={`${styles} text-base text-indigo-600 font-semibold tracking-wide uppercase`}>
+    <div className={`${styles} text-base text-main font-semibold tracking-wide uppercase`}>
         {subtitle}
     </div>
   )
