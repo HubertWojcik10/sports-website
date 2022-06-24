590b8cd935aae3644e21418bc8dcaf055df319c6
Hubert <hubert.wojcik002@gmail.com>
2022-06-24 12:04:19 +0200

    add seo to organizers

--- pages/organizatorzy.js
+++ pages/organizatorzy.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import OrganizersComp from '../components/_organizers/OrganizersComp'
+import Head from '../components/seo/Head'
 
 const Organizators = () => {
+  const props = {
+    'title': 'Organizatorzy Aktywności - Sports City',
+    'description': 'Chcesz dodawać swoje aktywności i eventy do aplikacji Sports City? Promuj swoją markę! Zarejestruj się tutaj.',
+    'img': 'logo.png',
+    'alt': 'Aplikacja Sports City',
+  }
+
   return (
     <>
+      <Head props={props} />
       <OrganizersComp />
     </>
   )
